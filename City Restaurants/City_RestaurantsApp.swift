962ac52d8200f1cdbf907bd9bb06d16c3e9530e9
Alexandru Ivanescu <alexbogdnivanescu@gmai.com>
2022-09-03 20:20:01 +0300

    Initial Commit

---
+++ City Restaurants/City_RestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_RestaurantsApp.swift
+//  City Restaurants
+//
+//  Created by Alex Ivanescu on 03.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct City_RestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
